873231b9a151ca852ecc7987089cf05e4ce0c581
yufu <vzheng56@gmail.com>
2015-10-18 23:19:24 +0800

    init
    添加工程

---
+++ SwiftAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAutoLayout
+//
+//  Created by 刘卫斌 on 15/10/18.
+//  Copyright © 2015年 yufu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
